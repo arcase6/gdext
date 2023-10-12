ceb5438675d8647f90d99b273efdbada9cd6c837
Jan Haller <bromeon@gmail.com>
2023-10-12 22:55:19 +0200

    (undo) short-circuit version

--- godot-bindings/src/godot_exe.rs
+++ godot-bindings/src/godot_exe.rs
@@ -100,6 +100,14 @@ fn update_version_file(version: &str) {
 */
 
 pub(crate) fn read_godot_version(godot_bin: &Path) -> GodotVersion {
+    // FIXME remove
+    // Read from env var CI_GODOT4_VERSION, if present
+    if let Ok(string) = std::env::var("GODOT_VERSION_CI") {
+        println!("Found GODOT_VERSION_CI env var with version: '{string}'");
+        println!("cargo:rerun-if-env-changed=GODOT_VERSION_CI");
+        return parse_godot_version(&string).expect("parse CI_GODOT4_VERSION");
+    }
+
     // On macOS, it spuriously happened since around Oct 2023, that the `--version` exited with signal: 11 (SIGSEGV).
     // This could only be reproduced with Rust's Command::new(), not when invoking the executable from bash.
     // Since I don't have time to debug rustc, let's retry a few times.
